20ae70b5d8c3d78cdba19d4c85b624a072d5ead5
Angie Keyser <angkeyb@gmail.com>
2018-04-25 21:43:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 function shout (string){
   return string.toUpperCase()
 }
+
+function whisper (string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function sayHiToGrandma(string){
+  var string
+  if(string.toLowerCase() === string) // true)
+  return("I can't hear you!")
+  else if (string === "I love you, Grandma."){
+    return ("I love you too")
+  }
+  else{
+    return("YES INDEED!")
+  }
+}
+
+
+function sayHiToGrandma(string){
+  var string
+  if
+  (string.toLowerCase() === string) // true)
+  return("I can't hear you!")
+  else if{
+  (string === "I love you, Grandma.")
+  return("I love you, too.")
+  } else{
+  return("YES INDEED!")
+}
